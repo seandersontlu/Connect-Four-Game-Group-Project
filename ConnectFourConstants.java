ec772e262f74b7d1495b52803ffd0f39e245845b
ajtate <47371946+ajtate@users.noreply.github.com>
2019-03-01 22:08:35 -0600

    Left out the port number

---
+++ ConnectFourConstants.java
@@ -0,0 +1,27 @@
+// Adrian, Sarah, Montrell, Scott
+// CSCI 434 Project 1
+// 2/27/19
+// ConnectFourConstants.java
+// 
+// This class is for the constants used for Connect Four.
+
+public interface ConnectFourConstants
+{
+    // Messages for the session and Client
+    public static final int START = 0;
+    public static final int PLAYER1 = 1;
+    public static final int PLAYER2 = 2;
+    public static final int PLAYER1_WON = 3;
+    public static final int PLAYER2_WON = 4;
+    public static final int PLAYER1_LOSS = 5;
+    public static final int PLAYER2_LOSS = 6;
+    public static final int WAIT_FOR_PLAYER = 7;
+    // public static final int MAKE_MOVE = 8;
+
+    // Used for end of game
+    public static final int CONNECT_FOUR = 2;
+    public static final int MAX_MOVES = 42;
+
+    // Port number
+    
+}
