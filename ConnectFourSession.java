03f00a1e37b3aa141c6602f7c184e4dbd93e4cb7
ajtate <47371946+ajtate@users.noreply.github.com>
2019-03-03 16:12:46 -0600

    Add files via upload

--- ConnectFourSession.java
+++ ConnectFourSession.java
@@ -30,8 +30,58 @@ public class ConnectFourSession implements ConnectFourConstants, Runnable
 			toPlayer1.writeInt(START);
 			toPlayer2.writeInt(START);
 
+            ConnectFourFrame myGui = new ConnectFourFrame();
+         
+            while (true)
+            {
+                // Player 1 makes a move
+                fromPlayer1.readInt();
+                
+                if (myGui.isWinner())
+                {
+                    if (myGui.getCurrentPlayer() == PLAYER_ONE)
+                    {
+                        toPlayer1.writeInt(PLAYER1_WON);
+                        toPlayer2.writeInt(PLAYER2_LOSS);
+                    }
+                    else
+                    {
+                        toPlayer2.writeInt(PLAYER2_WON);
+                        toPlayer1.writeInt(PLAYER1_LOSS);
+                    }
+                    sendMove(toPlayer2, col);
+                    break;
+                }
+                else if (myGui.isFull())
+                {
+                    toPlayer1.writeInt(MAX_MOVES);
+                    toPlayer2.writeInt(MAX_MOVES);
 
+                    sendMove(toPlayer2, col);
+                }
+                else
+                {
+                    toPlayer2.writeInt(CONTINUE);
+                    toPlayer1.writeInt(WAIT_FOR_PLAYER);
+
+                    sendMove(toPlayer2, col);
+                }
+
+            } 
+            player1.close();
+            player2.close();
+                
+		}
+		catch (Exception e)
+		{
+			System.err.println("error");
 		}
 	}
+
+    private void sendMove(DataOutputStream out, int col)
+        throws IOException
+    {
+       out.writeInt(col);
+    }
 }
 
