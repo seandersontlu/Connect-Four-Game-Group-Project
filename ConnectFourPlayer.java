9ac3c783bd2a09f508e32a30950dd9b30c534d18
seandersontlu <47400693+seandersontlu@users.noreply.github.com>
2019-03-06 10:01:47 -0600

    Add files via upload

--- ConnectFourPlayer.java
+++ ConnectFourPlayer.java
@@ -7,52 +7,92 @@
 // of wins, losses, ties for the game
 // connect four.
 
+/**
+ * This  class represents the player
+ * @author  Sarah, Adrian, Scott, and Montrel
+ */
+
 public class ConnectFourPlayer
 {
     private int numWins;
     private int numLosses;
+    private int numTies;
+    private int color;
     private String name;
     
-    public ConnectFourPlayer(String name)
+    /**
+     * @param name  players name
+     * @param color integer value for the asignned chips color
+     * Creates the player
+     */
+    public ConnectFourPlayer (String name, int color)
     {
         this.name = name;
-        numWins = 0;
-        numLosses = 0;
+        this.color = color;
     }
 
-    public int getNumWins()
-    {
-        return numWins;
-    }
-
-    public int getNumLosses()
-    {
-        return numLosses;
-    }
-
+    /** Returns the player's name
+     */
     public String getName()
     {
         return name;
     }
 
+    /** Returns the player's assigned color
+     */
+    public int getColor()
+    {
+        return color;
+    }
+
+    /** Returns the number of wins
+     */
+    public int getNumWins()
+    {
+        return numWins;
+    }
+
+    /** Returns the number of losses
+     */
+    public int getNumLosses()
+    {
+        return numLosses;
+    }
+    
+    /** Returns the number of ties
+     */
+    public int getNumTies()
+    {
+        return numTies;
+    }
+    
+    /** Increments the number of wins
+     */
     public void addWin()
     {
         numWins++;
     }
 
+    /** Increments the number of losses
+     */
     public void addLoss()
     {
         numLosses++;
     }
 
+    /** Increments the number of ties
+     */
+    public void addTie()
+    {
+        numTies++;
+    }
+
+    /** Resets the number of wins, losses, and ties to 0
+     */
     public void clearScore()
     {
         numWins = 0;
         numLosses = 0;
-    }
-
-    public String toString()
-    {
-        return (name + ": " + numWins + "/" + numLosses);
+        numTies = 0;
     }
 }
