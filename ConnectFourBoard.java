fb42152ce7a3ef82f414dd060129c7109046a0ca
ajtate <47371946+ajtate@users.noreply.github.com>
2019-03-06 23:56:23 -0600

    Added a getSlot method

--- ConnectFourBoard.java
+++ ConnectFourBoard.java
@@ -2,7 +2,7 @@
 // ************COMPLETED************
 // *********************************
 //
-// Sarah, Adrien, Montrell, Scott
+// Sarah, Adrien, Montrel, Scott
 // CSCI 434 Project #1, Iteration #3 
 // ConnectFourBoard.java
 // 3/06/2019
@@ -20,9 +20,9 @@ enum Diagonal
 /**
  * This class represents a connect four board
  * @author Jordyn Martin, Coy Tutt, David Glenewinkel II
- * @author Sarah, Adrian, Scott, Montrell
+ * @author Sarah, Adrian, Scott, Montrel
  */
-public class ConnectFourBoard implements ConnectFourConstants
+public class ConnectFourBoard
 {
     public static final int EMPTY = 0;
     public static final int YELLOW = 1;
@@ -30,21 +30,13 @@ public class ConnectFourBoard implements ConnectFourConstants
 
     public static final int COLUMNS = 7;
     public static final int ROWS = 6;
-    
     private int[][] gameBoard;
-    private ConnectFourPlayer player1;
-    private ConnectFourPlayer player2;
-    private int currentPlayer;
-
 
     /** Creates the connect four board
      */
-    public ConnectFourBoard(ConnectFourPlayer player1, ConnectFourPlayer player2)
+    public ConnectFourBoard()
     {
         gameBoard = new int[ROWS][COLUMNS];
-	this.player1 = player1;
-	this.player2 = player2;
-	currentPlayer = PLAYER_ONE;
         initializeBoard();
     }
     
@@ -67,31 +59,15 @@ public class ConnectFourBoard implements ConnectFourConstants
                 rowNum = row;
         return rowNum;
     }
-    
-    /**
-     * Returns the current player as an int for this turn
-     */
-    public int getCurrentPlayer()
-    {
-	int result = PLAYER_ONE;
-	if (currentPlayer == PLAYER_ONE)
-	    ;
-	else
-		result = PLAYER_TWO;
-	return result;
-    }
 
     /**
-     * Returns the current player name for this turn
+     * @param col the column number
+     * Returns the value of the given slot
      */
-    public String getCurrentPlayerName()
+    public int getSlot(int col)
     {
-	String result = "";
-	if (currentPlayer == PLAYER_ONE)
-		result = player1.getName();
-	else
-		result = player2.getName();
-	return result;
+	 // Temporary Statement
+         return gameBoard[getRowNum(col)][col];
     }
     
     /**
@@ -101,6 +77,20 @@ public class ConnectFourBoard implements ConnectFourConstants
     public boolean isColumnFull(int col)
     {
         return gameBoard[0][col] != EMPTY;
+    }
+	
+    /**
+     * Returns if the board is empty
+     */
+    public boolean isBoardFull()
+    {
+        boolean full = true;
+	for (int row = 0; row < ROWS; row++)
+	    for (int col = 0; col < COLUMNS; col++)
+	        if (gameBoard[row][col] == EMPTY)
+		    full = false;
+	
+	return full;
     }
 
     /** Initializes the board with the EMPTY constant
@@ -114,26 +104,13 @@ public class ConnectFourBoard implements ConnectFourConstants
     /**
      * @param col   the column number
      * @param color the integer value for the chip color
-     * adds chip to the given column and then switches
-     * the current player
+     * adds chip to the given column
      */
     public void drop(int col, int color)
     {
         for (int row = ROWS - 1; row >= 0; row--)
             if (gameBoard[row][col] == EMPTY)
                 gameBoard[row][col] = color;
-	switchPlayer();
-    }
-
-    /**
-     * Switches the current player
-     */
-    public void switchPlayer()
-    {
-	if (currentPlayer == PLAYER_ONE)
-		currentPlayer = PLAYER_TWO;
-	else
-		currentPlayer = PLAYER_ONE;
     }
     
     /** Resets the board
@@ -151,22 +128,6 @@ public class ConnectFourBoard implements ConnectFourConstants
             || diagonalWin(Diagonal.LEFT)
             || diagonalWin(Diagonal.RIGHT));
     }
-
-    /**
-     * Checks if the board is full
-     */
-    public boolean isFull()
-    {
-	boolean result = true;
-	for (int cols = 0; cols < COLUMNS; cols++)
-	{
-	    if (!isColumnFull(cols))
-		result = false;
-	}
-
-	return result;
-    }
-
 
     /** Checks for a vertical win
      */
